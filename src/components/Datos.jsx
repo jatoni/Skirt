3c995045d9271a695dad3c7ce0f6c106694d5c74
Juan Antonio Torres <95553786+jatoni@users.noreply.github.com>
2022-12-04 22:17:59 -0600

    Se ajusta la parte de circulo bar y al igual que las palabras que tendran

--- src/components/Datos.jsx
+++ src/components/Datos.jsx
@@ -4,30 +4,26 @@ import "react-circular-progressbar/dist/styles.css"
 
 const Datos = ({ usuario }) => {
     return (
-        <div className="w-full border border-orange-900 h-60">
-            <div className="flex justify-center items-center">
-                <div className="w-1/2 text-center">
-                    <div className="w-full h-4">
-                        <CircularProgressbar
-                            value={20}
-                            text={"20% de agua"}
-                        />
-                    </div>
-                </div>
-
-                <div className="w-1/2 text-center flex flex-nowrap flex-col justify-evenly">
-                    <p>Nombre de usuario</p>
-                    <p>Humedad de tierra</p>
-                    <p>Cantidad de veces que se riega el al dia</p>
-                </div>
-            </div>
-            {/* {usuario.map(user => (
-                <Dato 
-                    user={user}
-                    key={user.id}
+        <div className="contenedor contenedor-presupuesto sombra">
+            <div>
+                <CircularProgressbar
+                    value={30}
+                    text={"20% AGUA"}
+                    styles={buildStyles({
+                        pathColor: "#69b686",
+                        trailColor: "#f5f5f5",
+                        textColor: "#69b686",
+                        textSize: ".9rem"
+                    })
+                    }
                 />
-            )
-            )} */}
+            </div>
+
+            <div className="flex flex-col">
+                <p>Nombre de usuario</p>
+                <p>Humedad de tierra</p>
+                <p>Cantidad de veces que se riega el al dia</p>
+            </div>
         </div>
     )
 }
