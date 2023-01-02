39562969fe7d77d0b12a3ae7bd67443efca47337
Juan Antonio Torres <95553786+jatoni@users.noreply.github.com>
2023-01-02 06:21:20 -0600

    Ya se puede crear usuarios a la plataforma

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Opciones from "./components/Opciones";
-import Login from "./components/Login"
-import Registrar from "./components/Registrar"
+import Login from "./components/Login";
+import Registrar from "./components/Registrar";
+import Mensaje from "./components/Mensaje"
 
 const App = () => {
 
   const [error, setError] = useState(false);
   const [mensaje, setMensaje] = useState("")
   const [opciones, setOpciones] = useState(false);
+  const [usuarioCreado, setUsuarioCreado] = useState(false);
 
   const logIn = async (data) => {
     const url = "http://localhost:8080/Skirt/Login";
@@ -23,6 +25,24 @@ const App = () => {
     console.log(resultado);
   }
 
+  const registarUsuario = async (data) => {
+    const url = "http://localhost:8080/Skirt/Create";
+    const respuesta = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    });
+    const resultado = await respuesta.json();
+    if (resultado.codigo == 200) {
+      setUsuarioCreado(true);
+      setMensaje(`${resultado.mensaje}`)
+    } else {
+      setUsuarioCreado(false);
+    }
+  }
+
   return (
     <div>
       <Header />
@@ -33,7 +53,13 @@ const App = () => {
       <div className="w-full">
         {
           opciones ?
-            (<Registrar />)
+            (<>
+              {mensaje && <Mensaje>{mensaje}</Mensaje>}
+              <Registrar
+                registarUsuario={registarUsuario}
+                usuarioCreado={usuarioCreado} setMensaje={setMensaje}
+              />
+            </>)
             :
             (<Login
               logIn={logIn}
--- src/components/Mensaje.jsx
+++ src/components/Mensaje.jsx
@@ -1,8 +1,8 @@
 
 const Mensaje = ({ children }) => {
   return (
-    <div>
-        {children}
+    <div className="w-10/12 m-auto text-center p-10 border border-x-green-300 bg-green-300 rounded-md">
+        <h1 className={`text-5xl `}>{children}</h1>
     </div>
   )
 }
